92aafbfc7dd5a72a9aa529a96974a6994d2fff89
admin <mixagood>
2024-11-22 17:25:59 +0300

    WIP WIP

--- bot.js
+++ bot.js
@@ -8,20 +8,40 @@ const bot = new TelegramBot(token, { polling: true });
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
-const routes = {
-    'room:bookroom': (chatId) => {
-        bot.sendMessage(chatId, 'Бронирование комнаты')
-    },
-    'room': (chatId) => {
-        bot.sendMessage(chatId, 'Расписание комнаты')
-    }
-}
-
 const timeSlots = [];
 for (let i = 8; i < 21; i++) {
     timeSlots.push(i.toString() + "-00");
     timeSlots.push(i.toString() + "-30");
 }
+
+const routes = {
+    'book': async (chatId, roomId) => {
+        const jsonResponse = await bookRoom(access_token, );
+        const jsonStr = await JSON.stringify(jsonResponse);
+        bot.sendMessage(chatId, jsonStr);
+    },
+    'confirmbook' : (chatId) => {
+        bot.sendMessage(chatId, '');
+    },
+    'roomschedule': (chatId, room) => {
+
+        // get room schedule
+
+        buttons = timeSlots.map(item => {
+            return [{text: item, callback_data: ("booktime:" + `${arg};` + item)}]
+        });
+
+        const options = {
+            reply_markup: {
+                inline_keyboard: buttons,
+            },
+        };
+        
+        bot.sendMessage(chatId, 'Бронирование комнаты', options);
+    }
+}
+
+
 
 
 // Auth
@@ -54,12 +74,12 @@ async function getAuthToken() {
 }
 
 // Занятые комнаты
-async function getReservations(token) {
+async function getReservations(access_token) {
     
     const response = await fetch('http://127.0.0.1:8000/reservations', {
         method: 'GET',
         headers: {
-            'Authorization': `Bearer ${token}`,
+            'Authorization': `Bearer ${access_token}`,
             'Content-Type': 'application/json'
         }
     });
@@ -69,12 +89,12 @@ async function getReservations(token) {
     return jsonResponse;
 }
 
-async function getMeetingRooms(token) {
+async function getMeetingRooms(access_token) {
     
     const response = await fetch('http://127.0.0.1:8000/meeting_rooms', {
         method: 'GET',
         headers: {
-            'Authorization': `Bearer ${token}`,
+            'Authorization': `Bearer ${access_token}`,
             'Content-Type': 'application/json'
         }
     });
@@ -84,8 +104,7 @@ async function getMeetingRooms(token) {
     return jsonResponse;
 }
 
-
-async function bookRoom(token, roomId, fromReserve, toReserve) {
+async function bookRoom(access_token, roomId, fromReserve, toReserve) {
 
     const reqBody = {
         room_id : roomId,
@@ -97,7 +116,7 @@ async function bookRoom(token, roomId, fromReserve, toReserve) {
     const response = await fetch('http://127.0.0.1:8000/reservations/', {
         method: 'POST',
         headers: {
-            'Authorization': `Bearer ${token}`,
+            'Authorization': `Bearer ${access_token}`,
             'Content-Type': 'application/json',
         },
         body: JSON.stringify(reqBody)
@@ -116,20 +135,12 @@ async function bookRoom(token, roomId, fromReserve, toReserve) {
     }
 }
 
-
-async function getRooms() {
-
-    // const response = await fetch('http://127.0.0.1:8000/meeting_rooms/');
-    const jsonResponse = await getMeetingRooms();
-    // jsonResponse = [{"id":1, "descr": "205"}, {"id":2, "descr":"206"}, {"id":3, "descr":"207"}, {"id":4, "descr":"208"}]
-    return jsonResponse;
-}
-
 // Команда /start
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
   bot.sendMessage(chatId, 'Привет! Я бот для бронирования встреч в больнице 24!');
 });
+
 
 bot.onText(/\/roomlist/, async msg => {
 
@@ -156,27 +167,12 @@ bot.on('callback_query', (callbackQuery) => {
     const chatId = callbackQuery.message.chat.id;
     const data = callbackQuery.data;
 
-    const [action, arg] = data.split(':', 2);
+    const [route, param] = data.split(':');
 
-
-    if (action == 'room') {
-
-        buttons = timeSlots.map(item => {
-            return [{text: item, callback_data: ("booktime:" + `${arg};` + item)}]
-        });
-
-        const options = {
-            reply_markup: {
-                inline_keyboard: buttons,
-            },
-        };
-
-        bot.sendMessage(chatId, `Выбрана комната ${arg}. Свободные слоты:`, options);
-
-    } else if (action == 'booktime') {
-        const [room, time] = arg.split(';', 2);
-
-        bot.sendMessage(chatId, `Забронирована комната ${room} на время ${time}`);
+    if (routes[route]) {
+        routes[route](chatId, param);
+    } else {
+        bot.sendMessage(chatId, 'Неизвестная команда');
     }
 
 })
@@ -203,10 +199,10 @@ bot.onText(/\/mybookings/, async msg => {
 });
 
 
-bot.onText(/\/employees/, async msg => {
-    const chatId = msg.chat.id;
-    await bot.sendMessage(chatId, "Список сотрудников, с которыми можно попросить встречу (заглушка)");
-});
+// bot.onText(/\/employees/, async msg => {
+//     const chatId = msg.chat.id;
+//     await bot.sendMessage(chatId, "Список сотрудников, с которыми можно попросить встречу (заглушка)");
+// });
 
 // Error
 bot.on("polling_error", err => console.log(err.data.error.message));
@@ -219,16 +215,12 @@ const commands = [
         description: "Запуск бота"
     }, 
     {
-        command: "roomlist",
+        command: "displayrooms",
         description: "Список аудиторий"
     }, 
     {
         command: "mybookings",
         description: "Показать бронирования"
-    },
-    {
-        command: "employees",
-        description: "Сотрудники"
     },
     {
         command: "reservations",
